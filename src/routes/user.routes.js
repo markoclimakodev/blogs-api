7a4a38852d081c611c1e13fd8b95ca682f3596f0
markoclimako <mkclimako@gmail.com>
2023-11-01 00:47:50 -0300

    feat: add endpoint to list all users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,9 @@
 const route = require('express').Router();
 const { userController } = require('../controllers');
+const jwtTokenValidate = require('../middlewares/jwtTokenValidate');
 const validateUserRegister = require('../middlewares/validateUserRegister');
 
 route.post('/', validateUserRegister, userController.userRegistration);
+route.get('/', jwtTokenValidate, userController.getAllUsers);
 
 module.exports = route;
