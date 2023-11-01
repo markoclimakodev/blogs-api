f8c5ac90fe487e7a43377a8d5d05825fe4fb81c9
markoclimako <mkclimako@gmail.com>
2023-10-31 21:35:15 -0300

    feat: add login routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const { loginRoutes } = require('./routes');
 
 // ...
 
 const app = express();
+
+app.use(express.json());
 
 // não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use(express.json());
-
-// ...
+app.use('/login', loginRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
