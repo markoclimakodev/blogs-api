f79f009e82ef6b86452743ce3bad436ec9b03c34
markoclimako <mkclimako@gmail.com>
2023-10-31 22:33:47 -0300

    refactor: login service to use generateToken helper function

--- src/service/login.service.js
+++ src/service/login.service.js
@@ -1,16 +1,11 @@
-const jwt = require('jsonwebtoken');
 const { User } = require('../models');
+const generateToken = require('../utils/generateToken');
 
 const login = async (email, password) => {
   const user = await User.findOne({ where: { email, password },
     attributes: { exclude: ['email', 'password'] } });
 
-  if (!user) {
-    return null;
-  }
-  const { displayName } = user.dataValues;
-
-  const token = jwt.sign({ displayName }, process.env.JWT_SECRET);
+  const token = !user ? null : generateToken(user.id);
 
   return token;
 };
